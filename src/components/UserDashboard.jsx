5dcdcb0511d85dcc8eb77d96772e0486b614540e
Axel Calås <axel.calas.semst@gmail.com>
2025-01-09 13:37:46 +0100

    Removed feedback link from UserDashboard

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -38,7 +38,6 @@ function UserDashboard() {
       <LogoContainer src={Logo} />
       <Title>User Dashboard</Title>
       <Text>Welcome, {user}!</Text>
-      <Link to="/user/feedback" className="px-3 py-2 text-white bg-[#057d7a] rounded-lg hover:bg-[#2fadaa]">Leave feedback</Link>
       <Link to="/user/history" className="px-3 py-2 text-white bg-[#057d7a] rounded-lg hover:bg-[#2fadaa]">View History</Link>
       <Logout />
     </UserContainer>
