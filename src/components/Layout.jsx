87b940aefe1ffb8fc8a5713b04d48d7acec17384
Axel Calås <axel.calas.semst@gmail.com>
2025-01-13 14:05:24 +0100

    woops

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -28,8 +28,7 @@ const Layout = ({ children }) => {
                     {/* Burger Menu Button for Mobile */}
                     <button
                         onClick={toggleMenu}
-                        className="md:hidden text-[#057d7a] focus:outline-none"
-                    >
+                        className="md:hidden text-[#057d7a] focus:outline-none">
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                         </svg>
