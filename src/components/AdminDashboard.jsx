4338f53a3cd5755fb333accecfcdadd0b8ce26eb
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2025-01-16 16:16:43 +0100

    Added fetch on appointment and availability

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -5,9 +5,22 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// Existing function to fetch a single appointment by ID
+const getAvailability = async (authState) => {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/availability/${authState.userId}`,
+      {
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching availability:", error);
+    throw error;
+  }
+};
+
 const getAppointmentById = async (appointmentId) => {
-  console.log("Appointment ID:", appointmentId); // Debugging line
   try {
     const response = await axios.get(
       `${API_BASE_URL}/appointment/getappointmentbyid/${appointmentId}`,
@@ -15,7 +28,6 @@ const getAppointmentById = async (appointmentId) => {
         withCredentials: true,
       }
     );
-    console.log("Fetched Appointment:", response.data); // Debugging line
     return response.data;
   } catch (error) {
     console.error("Error fetching appointment:", error);
@@ -23,45 +35,54 @@ const getAppointmentById = async (appointmentId) => {
   }
 };
 
-// New function to fetch the user's appointments
-const getUserAppointments = async (userId) => {
-  try {
-    const response = await axios.get(
-      `${API_BASE_URL}/appointment/getappointmentsbyuserid/${userId}`,
-      {
-        withCredentials: true,
-      }
-    );
-    console.log("Fetched Appointments:", response.data); // Debugging line
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching appointments:", error);
-    throw error;
-  }
+const isDateInCurrentWeek = (date) => {
+  const now = new Date();
+  const startOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
+  const endOfWeek = new Date(now.setDate(now.getDate() + 6));
+
+  return date >= startOfWeek && date <= endOfWeek;
 };
 
 function AdminDashboard() {
   const { authState } = useAuth();
   const [appointments, setAppointments] = useState([]);
-  const rating = 3; // Dynamiskt betyg, ändra detta värde för att representera din användares betyg (1-5).
-  const progress = (rating / 5) * 100; // Beräkna procentsats för cirkeln
-  const {
-    authState: { user },
-  } = useAuth();
-  console.log("User object from useAuth:", authState.user); // Debugging line
+  const [availabilities, setAvailabilities] = useState([]);
+  const rating = 3;
+  const progress = (rating / 5) * 100;
 
   useEffect(() => {
-    if (user?.id) {
-      getUserAppointments(user.id)
+    if (authState.userId) {
+      getAvailability(authState)
         .then((data) => {
-          // Assuming the API returns all appointments, we slice to get the first 5
-          setAppointments(data.slice(0, 5));
+          const currentWeekAvailabilities = data.filter((availability) => {
+            const startTime = new Date(availability.startTime);
+            return isDateInCurrentWeek(startTime) && !availability.isBooked;
+          }).slice(0, 20);
+          setAvailabilities(currentWeekAvailabilities);
+
+          const bookedAvailabilities = data.filter((availability) => {
+            const startTime = new Date(availability.startTime);
+            return isDateInCurrentWeek(startTime) && availability.isBooked;
+          });
+
+          const fetchAppointments = bookedAvailabilities.map((availability) => {
+            return getAppointmentById(availability.appointmentId);
+          });
+
+          Promise.all(fetchAppointments)
+          .then((appointments) => {
+            console.log("Fetched Appointments: ", appointments);
+            setAppointments(appointments);
+          })
+          .catch((error) => {
+            console.error("Error fetching appointments:", error);
+          });
         })
         .catch((error) => {
-          console.error("Error fetching user appointments:", error);
+          console.error("Error fetching availability:", error);
         });
     }
-  }, [user]);
+  }, [authState]);
 
   return (
     <div className="flex flex-col items-center justify-center p-4">
@@ -69,19 +90,48 @@ function AdminDashboard() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
 
         {/* Card 1 */}
-        <div className="bg-white shadow-lg rounded-lg p-6">
-          <h3 className="text-xl font-semibold mb-2">Your Appointments</h3>
-          <p className="text-gray-600">
-            Here you can see some of your upcoming appointments.
-          </p>
+        <div className="bg-white shadow-lg rounded-lg p-4">
+          <h3 className="text-xl font-semibold mb-1">Your Appointments</h3>
+          <ul className="text-gray-600 mt-4 max-h-64 overflow-y-auto">
+          {appointments.length > 0 ? (
+  appointments.map((appointment, index) => (
+    <li key={index} className="mb-1 p-1 bg-gray-100 rounded-lg">
+      <span className="font-medium">
+        {appointment.patient.username} - {appointment.caregiver.username}
+      </span>
+      <span className="block text-sm">
+        {new Date(appointment.dateTime).toLocaleString()}
+      </span>
+      <span className="block text-sm">
+        Status: {appointment.status === 0 ? "Scheduled" : "Other"}
+      </span>
+    </li>
+  ))
+) : (
+  <li className="text-gray-500">No appointments for this week.</li>
+)}
+          </ul>
         </div>
 
         {/* Card 2 */}
-        <div className="bg-white shadow-lg rounded-lg p-6">
-          <h3 className="text-xl font-semibold mb-2">This Week Availabilities</h3>
-          <p className="text-gray-600">
-            View detailed reports and analytics to monitor system performance.
-          </p>
+        <div className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow duration-300">
+          <h3 className="text-xl font-semibold mb-1">This Week Availabilities</h3>
+          <ul className="text-gray-600 max-h-64 overflow-y-auto">
+            {availabilities.length > 0 ? (
+              availabilities.map((availability, index) => (
+                <li key={index} className="mb-1 p-2 bg-gray-100 rounded-lg">
+                  <span className="font-medium">
+                    {new Date(availability.startTime).toLocaleDateString()}
+                  </span>
+                  <span className="block text-sm">
+                    {new Date(availability.startTime).toLocaleTimeString()} - {new Date(availability.endTime).toLocaleTimeString()}
+                  </span>
+                </li>
+              ))
+            ) : (
+              <li className="text-gray-500">No availabilities for this day.</li>
+            )}
+          </ul>
         </div>
 
         {/* Card 3 */}
@@ -103,9 +153,9 @@ function AdminDashboard() {
               <circle
                 className="text-blue-500 progress-circle"
                 strokeWidth="4.5"
-                strokeDasharray={`${progress}, 100`} // Dynamic fill value
-                strokeDashoffset="0" // Set to 0 to ensure full fill
-                strokeLinecap="round" // Make the edges rounded
+                strokeDasharray={`${progress}, 100`}
+                strokeDashoffset="0"
+                strokeLinecap="round"
                 stroke="currentColor"
                 fill="none"
                 cx="18"
